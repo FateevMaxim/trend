16c1e1c2585c9a78bb6e4194725f4253d2c8cada
maximfateev <maxim.f@rocketfirm.com>
2023-11-15 00:12:23 +0600

    new address

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -49,7 +49,7 @@
 
                     <div class="bg-[#fab4b4] p-2 mt-4 mb-4">
                         <p><span>Образец</span></p>
-                        <img src="{{asset('images/newchina.jpg')}}" alt="China">
+                        <img src="{{asset('images/china.jpg')}}" alt="China">
                     </div>
 
                     <hr class="h-px my-8 bg-gray-200 border-0">
